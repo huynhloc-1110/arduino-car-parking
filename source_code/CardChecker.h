79336ae6c14b96ab0c67e41527d7fbdff1464dc6
huynhloc-1110 <le1999loc@gmail.com>
2022-08-26 08:49:39 +0700

    Add Comments to Code. Add CAD model for LCD and RFID.

--- source_code/CardChecker.h
+++ source_code/CardChecker.h
@@ -7,14 +7,23 @@
 class CardChecker {
   private:
     String *cardList;
+    // hold an array of card uid string
+    
     int cardNum;
+    // hold the number of card added to the card array
+    
     MFRC522 mfrc522;
+    // the object for controlling the rfid module
     
   public:
     CardChecker(int cardNum, byte ssPin, byte rstPin);
     void init();
     String readCard();
-    int checkCard(String card); // return -1 if not recognized, otherwise return card index
+    // use rfid module to read and return uid string
+    
+    int checkCard(String card);
+    // return -1 if not recognized, otherwise return card index
+    
     void addCard(String card);
     void removeCard(int cardIndex);
 };
